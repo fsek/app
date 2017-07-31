75aae51b08e3b1d538b41b9c640527a8269e8960
Jakob Navrozidis <jakob@navrozidis.com>
2017-07-31 20:47:54 +0200

    Login pages

--- www/js/login.js
+++ www/js/login.js
@@ -2,15 +2,32 @@
 $.auth.validateToken()
     .done(function() {
         afterSignIn();
+    })
+    .fail(function(){
+        loadLoginPage();
     });
 
-$$('.sign-in-btn').on('click', function () {
-    var email = $$('input[name="email"]').val();
-    var password = $$('input[name="password"]').val();
+myApp.onPageInit('login', function (page) {
+    $('.login-content input').on('input',function(e){
+        var email = $('input[name="login-email"]').val();
+        var password = $('input[name="login-password"]').val();
+        var loginBtn = $('.login-btn');
 
-    if(password === "" || email === ""){
-        myApp.alert("Please fill out both fields", "Login failed");
-    } else {
+        if(email != '' && password != ''){
+            if(loginBtn.hasClass('disabled')){
+                loginBtn.removeClass('disabled');
+            }
+        }else{
+            if(!loginBtn.hasClass('disabled')){
+                loginBtn.addClass('disabled');
+            }
+        }
+    });
+
+    $('.login-btn').on('click', function () {
+        var email = $('input[name="login-email"]').val();
+        var password = $('input[name="login-password"]').val();
+
         $.auth.emailSignIn({
             email: email,
             password: password
@@ -19,15 +36,40 @@ $$('.sign-in-btn').on('click', function () {
             afterSignIn();
         })
         .fail(function(resp) {
-            console.log('Authentication failure: ' + resp.reason, "Login failed");
+            myApp.alert("Ogiltig E-post eller lösenord", "Inloggningen misslyckades");
         });
-    }
+    });
+
+    $('.open-login-info').on('click', function () {
+        $('.login-info-container').animate({
+            "height": "+=100%",
+            "width": "+=100%"
+        }, 300);
+    });
+
+    $('.close-login-info').on('click', function () {
+        $('.login-info-container').animate({
+            "height": "-=100%",
+            "width": "-=100%"
+        }, 300);
+    });
 });
 
+function loadLoginPage(){
+    mainView.router.load({
+        url: 'login.html',
+        animatePages: false
+    });
+}
+
 function afterSignIn() {
-    myApp.closeModal('.login-screen');
+    $('#tab1 .cached').removeClass('cached');
+    $('.tabbar').show();
+    mainView.router.back({ //close login screen
+        pageName: 'tab1',
+        animatePages: false
+    });
     setupPush();
     initNotificationBadge();
     initCalendar();
 }
-

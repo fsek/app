ced6c3f8dce939664df569fd1c9e2c12ad9c56b8
Fredrik Lastow <fredrik.lw@hotmail.com>
2017-08-18 13:52:01 +0200

    login bug fix

---
+++ www/js/settings.js
@@ -0,0 +1,32 @@
+myApp.onPageInit('tab5', function(page){
+  $('.signout-btn').on('click', function(){
+    var signoutBtn = [
+      {
+        text: 'Är du säker på att du vill logga ut?',
+        label: true
+      },
+      {
+        text: 'Logga ut',
+        color: 'red',
+        onClick: function(){
+          $(calendar).empty();
+          deletePushAndSignOut();
+          tabView1.router.back({
+            animatePages: false
+          });
+          $('.toolbar-inner').find('.active').removeClass('active');
+          myApp.showTab('#login');
+          myApp.showTab('#subtab1');
+        }
+      }
+    ];
+    var cancel = [
+        {
+          text: 'Avbryt'
+        }
+    ];
+    var signoutAction = [signoutBtn, cancel];
+    myApp.actions(this, signoutAction);
+  });
+}).trigger();
+      

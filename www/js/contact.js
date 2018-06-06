d890a6b619a5fabfc26d3bc84f0a97320fd22cb7
Fredrik Lastow <fredrik.lw@hotmail.com>
2018-06-06 18:22:17 +0200

    contact page

--- www/js/contact.js
+++ www/js/contact.js
@@ -1,101 +1,109 @@
-myApp.onPageInit('contact', function(page) {
+$$(document).on('page:init', '.page[data-name="contact"]', function (e) {
   $.getJSON(API + '/contacts')
     .done(function(resp) {
       var contacts = resp.contacts;
-      initContactSelector(contacts, page);
-      setDescription(contacts);
-      $('.contact-button').on('click', function() {
-        sendMessage(contacts, page);
-      });
-      // Event when content in div changes
-      $('#contact-select .item-after').bind("DOMSubtreeModified", function() { 
-        setDescription(contacts);
-      });
-      $('.contact-content .infinite-scroll-preloader').remove();
+      initContactPage(contacts, e);
     })
     .fail(function(resp) {
       console.log(resp.statusText);
     });
 });
 
-function initContactSelector(contacts, page) {
-  var selectedId = page.query.id;
+function initContactPage(contacts, event) {
+  // Add the contact options for the smart select and save the selected contact in the contacts object
+  addContactOptions(contacts, event);
 
-  // Set Spindelman as default contact if not linked from an event
-  if (selectedId == undefined) {
-    selectedId = 1;
-  }
+  // Init the smart select with the added options 
+  initSmartSelect(contacts)
+  
+  // Add the selected contact's name and descrition to the page
+  setDescription(contacts.selected_contact);
 
-  // Find name of the selected contact
-  // contact.selected is set to true if the contact is the selected one, otherwise false
-  var selectedName = '';
-  for (contact of contacts) {
-    if (contact.id == selectedId) {
-      contact.selected = true;
-      selectedName = contact.name;
-    } else {
-      contact.selected = false;
-    }
-  }
-  // Load the template and the HTML for the contact selector
-  var templateHTML = myApp.templates.contactTemplate({ contact: contacts, selectedName: selectedName });
-  var contactContent = $('#contact-select');
-  contactContent.html(templateHTML);
-}
+  $('.contact-button').on('click', function() {
+    sendMessage(contacts.selected_contact);
+  });
+
+  $('.contact-content .infinite-scroll-preloader').remove();
 
 
-function sendMessage(contacts, page) {
-  // Get data from form
-  var formData = myApp.formToData('#contact-form');
+  function addContactOptions(contacts, event) {
+    var selectedId = event.detail.route.params.contactId;
 
-  // If the textarea is filled 
-  if (formData.message != '') {
-    var user = $.auth.user;
-    var contactData = { contact_message: {} }
-    contactData.contact_message.name = user.firstname + ' ' + user.lastname;
-    contactData.contact_message.email = user.email;
-    contactData.contact_message.message = formData.message;
-
-    // Find name of selected contact
-    var contactName = $('select[name="contacts"] option:checked').val();
-
-    // Find id of selected contact
+    // Find which option is selected and save that contact in the contacts object 
+    // contact.selected is set to true if the contact is the selected one, otherwise false
+    var selectedName = '';
     for (contact of contacts) {
-      if (contact.name == contactName) {
-        var contactId = contact.id;
+      contact.selected = contact.id == selectedId;
+
+      if(contact.selected) {
+        contacts.selected_contact = contact;
       }
     }
 
-    // Send request to send e-mail
-    $.ajax({
-      type: 'POST',
-      dataType: 'json',
-      url: API + '/contacts/' + contactId + '/mail',
-      data: contactData,
-      success: function(resp) {
-        myApp.alert('Tack för att du har kontaktat oss! Ha en fin dag! :)', 'Tack!', function() {
-          $('.contact-text textarea').val('');
-        });
-      },
-      fail: function(resp) {
-        myApp.alert('Ojdå! Det gick inte att skicka mailet, prova igen senare. :(', 'Felmeddelande!');
+    // Load the template and the HTML for the contact options
+    var templateHTML = app.templates.contactTemplate({ contact: contacts });
+    $('#contact-select').html(templateHTML);
+  }
+
+  function initSmartSelect(contacts){
+    app.smartSelect.create({
+      el: '#contact-select',
+      openIn: 'popup',
+      popupCloseLinkText: 'Tillbaka',
+      searchbar: true,
+      searchbarPlaceholder: 'Sök efter kontaktperson',
+      searchbarDisableText: 'Avbryt',
+      closeOnSelect: true,
+      on: {
+        close: function(smartSelect) {
+          var selectedName = $('#contact-select .item-after')[0].innerHTML;
+          contacts.forEach(function(contact){
+            if(contact.name == selectedName) {
+              contacts.selected_contact = contact;
+            }
+          });
+
+          setDescription(contacts.selected_contact);
+        },
       }
     });
-
-  // If the textarea is empty
-  } else {
-    myApp.alert('Du har inte skrivit något meddelande', 'Inget meddelande')
   }
-}
 
-function setDescription(contacts) {
-  var contactName = $('select[name="contacts"] option:checked').val();
-  var selectedText;
-  for (contact of contacts) {
-    if (contact.name == contactName) {
-      selectedText = contact.text;
+  function setDescription(selectedContact) {
+    console.log(selectedContact)
+    $('#contact-description-title').html(selectedContact.name);
+    $('#contact-description').html(selectedContact.text);
+    $('#contact-email').html('<span>E-post: </span>' + selectedContact.email);
+  }
+
+  function sendMessage(selectedContact) {
+    // Get data from form
+    var formData = app.form.convertToData('#contact-form');
+
+    // If the textarea is filled 
+    if (formData.message != '') {
+      var user = $.auth.user;
+      var contactData = { contact_message: {} }
+      contactData.contact_message.name = user.firstname + ' ' + user.lastname;
+      contactData.contact_message.email = user.email;
+      contactData.contact_message.message = formData.message;
+
+      // Send POST request to send e-mail
+      $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        url: API + '/contacts/' + selectedContact.id + '/mail',
+        data: contactData,
+        success: function(resp) {
+          $('.contact-text textarea').val('');
+          app.dialog.alert('Tack för att du har kontaktat oss! Vi kommer svara så snabbt vi kan. Ha en fortsatt fin dag! :)', 'Meddelande skickat');
+        },
+        fail: function(resp) {
+          app.dialog.alert('Ojdå! Det gick inte att skicka mailet, prova igen senare. :(', 'Felmeddelande!');
+        }
+      });
+    } else {
+      app.dialog.alert('Du har inte skrivit något meddelande', 'Inget meddelande')
     }
   }
-  $('#contact-description-title').html(contactName);
-  $('#contact-description').html(selectedText);
 }

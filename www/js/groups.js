168950238817cf70c9edfd72abdeecaf9eb2cd40
Fredrik Lastow <fredrik.lw@hotmail.com>
2018-08-24 14:45:55 +0200

    Nollning tab final touches

--- www/js/groups.js
+++ www/js/groups.js
@@ -18,8 +18,25 @@ function getGroups() {
           hasGroups: hasGroups});
         groupEl.addClass('no-groups');
       }
-      
+
       groupEl.html(templateHTML);
+      updateGroupBadge(unread);
+    });
+}
+
+// Only a function for the nollning page for the group notification badge. We throw away some data here, but its only temporary
+function setGroupNotification() {
+  $.getJSON(API + '/groups')
+    .then(function(resp) {
+      const groups = resp.groups.reverse();
+      let unread = 0;
+      const hasGroups = groups.length !== 0;
+      if (hasGroups) {
+        for (var group of groups) {
+          unread += group.group_user.unread_count;
+        }
+      }
+
       updateGroupBadge(unread);
     });
 }
@@ -28,7 +45,6 @@ $$(document).on('page:init', '.page[data-name="groups"]', function (e) {
   // Get messages if we haven't done so already
   if ($$('#groups-list ul').is(':empty')) getGroups();
 });
-
 /*
 $$('#view-groups').on('tab:show', function() {
   // Get messages if we haven't done so already
